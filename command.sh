bafa4e6062bca065f3c7822c9d1a77e5ad7b4caa
Zeyang Zhang(Outsourcing) <zeyang.a.zhang@univers.com>
2025-03-07 14:36:33 +0800

    second

--- command.sh
+++ command.sh
@@ -3,3 +3,5 @@ kubectl get deployments.apps -n fc -o custom-columns='NAME:metadata.name,REPLICA
 
 # old version get all image for one ns.
 kubectl get deployments -n gssc -o=jsonpath='{range .items[*]}{.metadata.name}: {.spec.template.spec.containers[*].image}{"\n"}{end}'
+
+kubectl get pod -A | grep -v Running | awk '{print"kubectl delete pod -n" $1 " " $2}'
